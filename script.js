82f589de69cf2c7d22e2c303ddd32da62b367603
aaliahsaba <aaliahsaba25@gmail.com>
2022-04-19 23:11:07 +0400

    New files

---
+++ script.js
@@ -0,0 +1,9 @@
+// var colorSwitch = document.getElementById('colorSwitch');
+// colorSwitch.addEventListener('change', () => {
+// 	document.body.classList.toggle('dark');
+// });
+
+function myFunction() {
+	var element = document.body;
+	element.classList.toggle("dark-mode");
+ }
