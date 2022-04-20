43392dad6a015050c6bca039b61b1aa7f28dd8f3
aaliahsaba <104029178+aaliahsaba@users.noreply.github.com>
2022-04-20 15:23:52 +0400

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// var colorSwitch = document.getElementById('colorSwitch');
-// colorSwitch.addEventListener('change', () => {
-// 	document.body.classList.toggle('dark');
-// });
-
-function myFunction() {
-	var element = document.body;
-	element.classList.toggle("dark-mode");
- }
